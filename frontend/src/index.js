35fc91939157e2561434599b121b52ec55d07e1a
ssy02060 <ssy02060@gmail.com>
2022-07-29 11:09:08 -0700

    react 추가

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,26 @@
-var express = require('express'); // 설치한 express module을 불러와서 변수(express)에 담습니다.
-var app = express(); //express를 실행하여 app object를 초기화 합니다.
+const express = require('express');
+const path = require("path");
+const http = require("http");
+
+const app = express();
+
+const REACT_PATH = '../frontend-react/'
+
+app.use(express.static(path.join(__dirname, REACT_PATH + 'build')))
+
+function setupHandlers(app) {
+    app.set()
+}
 
 app.get('/', function (req, res) { // '/' 위치에 'get'요청을 받는 경우,
-    res.send('Hello fffff!'); // "Hello World!"를 보냅니다.
+    res.sendFile(path.join(__dirname, REACT_PATH + 'build/index.html'))
 });
 
-var port = 3000; // 사용할 포트 번호를 port 변수에 넣습니다. 
+app.get('*', function (req, res) { // '/' 위치에 'get'요청을 받는 경우,
+    res.sendFile(path.join(__dirname, REACT_PATH + 'build/index.html'))
+});
+
+const port = process.env.PORT;
 app.listen(port, function () { // port변수를 이용하여 3000번 포트에 node.js 서버를 연결합니다.
     console.log('server on! http://localhost:' + port); //서버가 실행되면 콘솔창에 표시될 메세지입니다.
 });
