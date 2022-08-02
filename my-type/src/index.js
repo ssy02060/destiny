c4477123210543730fde7e6b3ae825c19b3a967d
matthew0507 <cnrrnqhk@gmail.com>
2022-08-02 11:20:35 +0900

    tag에서 my-type으로 변경

--- my-type/src/index.js
+++ my-type/src/index.js
@@ -3,7 +3,7 @@ var express = require('express');
 //express를 실행하여 app object를 초기화 합니다.
 var app = express();
 app.get('/', function (req, res) { // '/' 위치에 'get'요청을 받는 경우,
-    res.send('tag service open!'); // "Hello World!"를 보냅니다.
+    res.send('my-type open!'); // "Hello World!"를 보냅니다.
 });
 var port = 3000; // 사용할 포트 번호를 port 변수에 넣습니다.
 app.listen(port, function () { // port변수를 이용하여 3000번 포트에 node.js 서버를 연결합니다.
