c85320f2595a71465bb2121a2c304508656839a3
ssy02060 <ssy02060@gmail.com>
2022-08-03 10:36:06 -0700

    frontend 초기화

--- gateway/src/index.js
+++ gateway/src/index.js
@@ -1,50 +1,14 @@
-const express = require('express');
-const path = require("path");
-const http = require("http");
-const AWS = require("aws-sdk");
-const app = express();
+const mongodb = require("mongodb");
 
-const port = process.env.PORT;
-const REACT_PATH = process.env.REACT_PATH;
-const DBHOST = process.env.DBHOST;
-
-AWS.config.update({
-    region: "us-west-2",
-    endpoint: DBHOST
-})
-
-const dynamodb = new AWS.DynamoDB();
-
-const params = {
-    TableName: "test",
-    KeySchema: [
-        {
-            AttributeName: "email", KeyType: "HASH"
-        }],
-    AttributeDefinitions: [
-        {
-            AttributeName: "email", AttributeType: "S"
-        }],
-    ProvisionedThroughput: {
-        ReadCapacityUnits: 5,
-        WriteCapacityUnits: 5
-    }
+function connectDb(dbHost, dbName) {
+    return mongodb.MongoClient.connect(dbHost, { useUnifiedTopology: true })
+        .then(client => {
+            const db = client.db(dbName);
+            return {                // Return an object that represents the database connection.
+                db: db,             // To access the database...
+                close: () => {      // and later close the connection to it.
+                    return client.close();
+                },
+            };
+        });
 }
-
-
-app.get('/', function (req, res) { // '/' 위치에 'get'요청을 받는 경우,
-    // res.sendFile(path.join(__dirname, REACT_PATH + 'build/index.html'))
-    res.send("main page");
-});
-
-app.get('/create-table', function (req, res) { // '/' 위치에 'get'요청을 받는 경우,
-    // res.sendFile(path.join(__dirname, REACT_PATH + 'build/index.html'))
-    dynamodb.createTable(params, console.log);
-    res.send("main page");
-});
-
-
-
-app.listen(port, function () { // port변수를 이용하여 3000번 포트에 node.js 서버를 연결합니다.
-    console.log('server on! http://localhost:' + port); //서버가 실행되면 콘솔창에 표시될 메세지입니다.
-});
--- python-web/src/app.py
+++ python-web/src/app.py
@@ -1,11 +1,58 @@
 from flask import Flask
+from gridfs import Database
+from pymongo import MongoClient
+import os
+import datetime
+import pprint
+post = {"id": "Mike",
+        "email": "My first blog post!",
+        "tags": ["mongodb", "python", "pymongo"],
+        "date": datetime.datetime.utcnow()
+        }
 
 app = Flask("api_test")
 
-@app.route('/')
+PORT = os.environ['PORT']
+DBHOST = os.environ['DBHOST']
+
+
+@ app.route('/')
 def hello():
-    return 'Hello'
+    return 'python-web'
+
+
+post_id = ''
+
+
+@ app.route('/create-collection')
+def create_database():
+    client = MongoClient(host=DBHOST, port=27017)
+    # 각자 서비스명
+
+    db = client['movie-data']
+    movie_data = db['movie_data']
+    post_id = movie_data.insert_one(post).inserted_id
+
+    print(post_id)
+
+    return 'inserted'
+
+
+@ app.route('/query')
+def query():
+    client = MongoClient(host=DBHOST, port=27017)
+    # 각자 서비스명
+
+    db = client['python-web']
+    posts = db.posts
+    pprint.pprint(posts.find_one({"_id": post_id}))
+
+    item = posts.find_one({"_id": post_id})
+
+    print(post_id)
+
+    return 'item' + post_id
+
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
-
+    app.run('0.0.0.0', port=PORT, debug=True)
