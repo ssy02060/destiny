fbdcaf6f7d7b99cf9a2897bbb7a753629873bf1a
eukgun <eukgun@gmail.com>
2022-08-04 20:36:58 +0900

    review_list

--- review/src/app.py
+++ review/src/app.py
@@ -2,31 +2,26 @@ from flask import Flask
 from pymongo import MongoClient
 import os
 import datetime
-
-post = {"author": "Mike",
-        "text": "My first blog post!",
-        "tags": ["mongodb", "python", "pymongo"],
-        "date": datetime.datetime.utcnow()
-        }
-
+post = {
+    "user": "Eukgun",
+    "movie": "Harry Potter",
+    "review": {
+                "comment": "Hmmm",
+                "rate": 3
+          }
+          }
 app = Flask("api_test")
-
 PORT = os.environ['PORT']
 DBHOST = os.environ['DBHOST']
-'''
-@app.get('/review')
-def review_list():
-    return 'Review'
-'''    
-def withUser(userid):
+@app.route('/')
+def hello():
+    return 'Review-list'
+@app.route('/review')
+def create_database():
     client = MongoClient(host=DBHOST, port=27017)
-
     db = client['review']
     posts = db.review
     post_id = posts.insert_one(post).inserted_id
-
     return 'created'
-#def withMoviCd(movieCd):
-
 if __name__ == '__main__':
     app.run('0.0.0.0', port=PORT, debug=True)
