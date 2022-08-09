156fafa18a11dfd3b95b8f361b65745b6e287485
ssy02060 <ssy02060@gmail.com>
2022-08-08 19:01:55 -0700

    main page, review page 최초 코드

--- gateway/ui/src/App.js
+++ gateway/ui/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './pages/Main';
+import Review from './pages/Review';
+import NotFound from './pages/NotFound';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className='App'>
+            <BrowserRouter>
+                {/* <Header /> */}
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Main />}></Route>
+                    <Route path="/review/*" element={<Review />}></Route>
+                    {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
+                    <Route path="*" element={<NotFound />}></Route>
+                </Routes>
+            </BrowserRouter>
+
+        </div>
+    );
 }
 
 export default App;
