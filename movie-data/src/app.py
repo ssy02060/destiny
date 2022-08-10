8ecf8d023c12c709d0c07c34bb8a0aaace89bb3d
joomankim <wnaks42@naver.com>
2022-08-09 17:26:30 -0700

    [movie-data] 네이버 영화 크롤링

--- movie-data/src/app.py
+++ movie-data/src/app.py
@@ -19,20 +19,20 @@ def root():
         parameters += 'key: {}, value: {}\n'.format(key, request.args[key])
     return 'Movie API'
 
-@app.route('/movie')
-def year_find_movie():
-    client = MongoClient(host=DBHOST, port=27017)
-    mydb = client['movie_data']
+# @app.route('/movie')
+# def year_find_movie():
+#     client = MongoClient(host=DBHOST, port=27017)
+#     mydb = client['movie_data']
 
-    parameter_dict = request.args.to_dict()
-    if len(parameter_dict) == 0:
-        return 'No parameter'
+#     parameter_dict = request.args.to_dict()
+#     if len(parameter_dict) == 0:
+#         return 'No parameter'
 
-    parameters = ''
-    for key in parameter_dict.keys():
-        parameters += 'key: {}, value: {}\n'.format(key, request.args[key])
+#     parameters = ''
+#     for key in parameter_dict.keys():
+#         parameters += 'key: {}, value: {}\n'.format(key, request.args[key])
     
-    return mydb.movieInfo.delete_many({"openYear":request.args[key]})
+#     return mydb.movieInfo.find({"openYear":request.args[key]})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=PORT, debug=True)
