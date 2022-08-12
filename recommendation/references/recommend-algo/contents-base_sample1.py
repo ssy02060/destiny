2e093f274b8837bbbac53db873889378aa63f8cc
joomankim <wnaks42@naver.com>
2022-08-11 19:18:08 -0700

    [recommendation] (references) contents-base 코드

--- recommendation/references/recommend-algo/contents-base_sample1.py
+++ recommendation/references/recommend-algo/contents-base_sample1.py
@@ -1,6 +1,6 @@
 # https://romg2.github.io/mlguide/01_%EB%A8%B8%EC%8B%A0%EB%9F%AC%EB%8B%9D-%EC%99%84%EB%B2%BD%EA%B0%80%EC%9D%B4%EB%93%9C-09.-%EC%B6%94%EC%B2%9C%EC%8B%9C%EC%8A%A4%ED%85%9C-%EC%BD%98%ED%85%90%EC%B8%A0-%EA%B8%B0%EB%B0%98/
 # 추천 시스템 [컨텐츠 기반]
-# pip insatll numpy, pandas, matplotlib, seaborn
+# pip insatll numpy, pandas, matplotlib, seaborn, sklearn
 import numpy as np
 import pandas as pd
 
