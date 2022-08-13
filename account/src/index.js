b84138212e0d80680e4210c0e0db73d15bb190f1
noosery <seri1749@naver.com>
2022-08-13 13:30:39 +0900

    [account] 로그인 api 환경구성

--- account/src/index.js
+++ account/src/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 app = express();
 const PORT = process.env.PORT;
+
+//
 app.get('/', function (req, res) {
     res.send("hello from account service!!");
 });
+
+app.get('/hello', function (req, res) {
+    res.send("hello");
+});
+
+app.get('/login', function (req, res) {
+    res.send("login successfully");
+});
+//client로 요청을 기다린다
 app.listen(PORT, function () {
     console.log('server on! ' + PORT);
 });
