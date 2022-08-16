46b750bb2dde258b572e38ab88853b25a539e1cb
eukgun <eukgun@gmail.com>
2022-08-15 22:25:26 -0700

    [aws-storage]이미지 다운로드 및 연결 확인

--- aws-storage/src/app.py
+++ aws-storage/src/app.py
@@ -6,10 +6,9 @@ AWS_ACCESS_KEY = os.environ['AWS_ACCESS_KEY']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 REGION = os.environ['REGION']
 
-def s3_connection():
+def s3_session():
     try:
-        s3 = boto3.client(
-            service_name="s3",
+        session = boto3.Session(
             region_name=REGION,
             aws_access_key_id=AWS_ACCESS_KEY,
             aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
@@ -18,27 +17,30 @@ def s3_connection():
         print(e)
     else:
         print("s3 bucket connected!")
-        return s3
+        return session
 
-def s3_put_object(s3, bucket, filepath, access_key):
-    """
-    s3 bucket에 지정 파일 업로드
-    :param s3: 연결된 s3 객체(boto3 client)
-    :param bucket: 버킷명
-    :param filepath: 파일 위치
-    :param access_key: 저장 파일명
-    :return: 성공 시 True, 실패 시 False 반환
-    """
-    try:
-        s3.upload_file("{로컬에서 올릴 파일이름}","{버킷 이름}","{버킷에 저장될 파일 이름}")
-    except Exception as e:
-        print(e)
+# def s3_put_object(s3, bucket, filepath, access_key):
+#     """
+#     s3 bucket에 지정 파일 업로드
+#     :param s3: 연결된 s3 객체(boto3 client)
+#     :param bucket: 버킷명
+#     :param filepath: 파일 위치
+#     :param access_key: 저장 파일명
+#     :return: 성공 시 True, 실패 시 False 반환
+#     """
+#     try:
+#         s3.upload_file("{로컬에서 올릴 파일이름}","{버킷 이름}","{버킷에 저장될 파일 이름}")
+#     except Exception as e:
+#         print(e)
 
-s3 = s3_connection()
+s3 = s3_session().resource('s3')
 def read_image_from_s3(filename):
-    s3
+    bucket = s3.Bucket("dstny.movie.poster")
+    object = bucket.Object(filename)
+    response = object.get()
     file_stream = response['Body']
+
     img = Image.open(file_stream)
     return img
 
-read_image_from_s3('191139.png')
+print(read_image_from_s3('191139.png'))
