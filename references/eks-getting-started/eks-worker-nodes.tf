213523a05358193d81995581f90a7a123d1ea959
ssy02060 <ssy02060@gmail.com>
2022-08-29 16:55:29 +0000

    [Infra] backend, replica set 수정

--- references/eks-getting-started/eks-worker-nodes.tf
+++ references/eks-getting-started/eks-worker-nodes.tf
@@ -45,8 +45,8 @@ resource "aws_eks_node_group" "destiny" {
   subnet_ids      = aws_subnet.destiny[*].id
 
   scaling_config {
-    desired_size = 1
-    max_size     = 1
+    desired_size = 2
+    max_size     = 2
     min_size     = 1
   }
 
